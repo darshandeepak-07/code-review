5f67c5b114774d67b3e2f540b439107208263234
DeepakDarshan <darshandeepak1483@gmail.com>
2024-01-30 22:16:53 +0530

    Changes

--- code review app/app.js
+++ code review app/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require("express");
 const app = express();
 const authRouter = require("./routes/authentication/authentication");
@@ -12,26 +11,3 @@ app.listen(PORT, (error) => {
   if (error) console.log(error);
   console.log("server started...");
 });
-=======
-const express = require('express');
-require('dotenv').config();
-const app = express();
-
-
-// Declaring routes
-const userRoutes = require('./routes/users/user')
-const authRoutes = require('./routes/authentication/authentication')
-
-app.get('/',(req,res)=>{
-    
-})
-// Using Routes
-app.use('/users',userRoutes);
-
-
-
-const PORT = process.env.PORT || 8055;
-app.listen(PORT,()=>{
-    console.log("Server is live on 8000")
-})
->>>>>>> 1b61622a3389f3188424136e4fc0ad77ea57e494
