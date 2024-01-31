e26572954fe5a5feae324e79524dd85810ba172d
DeepakDarshan <darshandeepak1483@gmail.com>
2024-01-31 20:16:24 +0530

    Added User Profile Editting

--- code review app/routes/user/user.js
+++ code review app/routes/user/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {findUser,editUser} = require('../../controller/user/user');
+const {findUser,editUser,pwdEdit} = require('../../controller/user/user');
 const bodyParser = require("body-parser");
 
 router.use(bodyParser.json());
@@ -10,4 +10,5 @@ router.use(bodyParser.urlencoded({ extended: true }));
 
 router.get('/:userId',findUser);
 router.put('/:userId/edit',editUser)
+router.put('/:userId/edit/password',pwdEdit)
 module.exports = router;
